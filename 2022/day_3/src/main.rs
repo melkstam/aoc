c99a1a55ac279a5a4b783849e550e3786061f0c0
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2022-12-04 11:23:29 +0100

    2022, day 3

--- 2022/day_3/src/main.rs
+++ 2022/day_3/src/main.rs
@@ -1,8 +1,16 @@
 use std::io;
 
 fn main() {
-    part1();
-    // part2();
+    //part1();
+    part2();
+}
+
+fn get_priority(c: char) -> i32 {
+    if c.is_ascii_lowercase() {
+        return (c as i32) - ('a' as i32) + 1;
+    }
+
+    return (c as i32) - ('A' as i32) + 1 + 26;
 }
 
 fn part1() {
@@ -22,14 +30,32 @@ fn part1() {
 
             return 'a';
         })
-        .map(|c| {
-            if c.is_ascii_lowercase() {
-                return (c as i32) - ('a' as i32) + 1;
-            }
-
-            return (c as i32) - ('A' as i32) + 1 + 26;
-        })
+        .map(|c| get_priority(c))
         .sum();
 
     println!("{sum}");
+}
+
+fn part2() {
+    let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        .chars()
+        .collect();
+
+    let lines: Vec<_> = io::stdin().lines().map(|l| l.unwrap()).collect();
+
+    let sum: i32 = lines
+        .chunks(3)
+        .map(|c| {
+            for d in alphabet.clone() {
+                if c[0].contains(d) && c[1].contains(d) && c[2].contains(d) {
+                    return d;
+                }
+            }
+
+            'a'
+        })
+        .map(|c| get_priority(c))
+        .sum();
+
+    println!("{sum}, {}", ('A'..='z').count());
 }
